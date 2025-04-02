df6c80205fe96a49ea2d14973cd8c46c411c5231
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-02 13:58:16 +0200

    Fix  avatar not displayed before moving

--- Klawiatury/Program.cs
+++ Klawiatury/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.CursorVisible = false;
 Player hero = new Player("Snake");
 
-Console.WriteLine($"({hero.x}, {hero.y})");
 hero.speed = 3;
 hero.x = 119;
 hero.y = 3;
@@ -14,6 +13,12 @@ directionsMap.Add(ConsoleKey.S, new Point(0, hero.speed));
 
 while (true)
 {
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"({hero.x}, {hero.y})     ");
+
+    Console.SetCursorPosition(hero.x, hero.y);
+    Console.Write("@");
+    
     ConsoleKeyInfo pressedKeyInfo = Console.ReadKey(true);
 
     Console.SetCursorPosition(hero.x, hero.y);
@@ -29,12 +34,6 @@ while (true)
         hero.x = Math.Clamp(hero.x, 0, Console.BufferWidth - 1);
         hero.y = Math.Clamp(hero.y, 0, Console.BufferHeight - 1);
     }
-    
-    Console.SetCursorPosition(0, 0);
-    Console.WriteLine($"({hero.x}, {hero.y})     ");
-
-    Console.SetCursorPosition(hero.x, hero.y);
-    Console.Write("@");
 }
 
 Console.WriteLine("Press Space to continue...");
