ac16fa7050cf37ce584f73b631977e3d6128c05f
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-16 15:07:44 +0200

    Fix walking through walls with high speed

--- Klawiatury/Program.cs
+++ Klawiatury/Program.cs
@@ -15,11 +15,11 @@ string[] level =
 [
     "########################################",
     "#......................................#",
-    "#.................................######",
+    "#...#.............................######",
     "#...............#...................#",
     "#.................................###",
     "#.................................#",
-    "#.................................#",
+    "#...#.............................#",
     "#.................................#",
     "#.................................#",
     "#.................................#",
@@ -56,22 +56,54 @@ while (true)
 
         foreach (Player element in clones)
         {
-            int targetX = element.position.x + direction.x * element.speed;
-            int targetY = element.position.y + direction.y * element.speed;
-            if (level[targetY][targetX] == '#')
+            // 1. Załóżmy, że docelowa pozycja początkowo to moja aktualna pozycja
+            // 2. Róbmy krok po kroku w wybranym kierunku
+            // 3. Przy każdym kroku sprawdźmy czy nie próbujemy wejść w ścianę
+            // 4a. Jeśli tak (planujemy wejść w ścianę), to przerwij
+            // 4b. Jeśli nie, to zaktualizuj docelową pozycję
+
+            int targetX = element.position.x;
+            int targetY = element.position.y;
+
+            int signY = Math.Sign(direction.y);
+            int signX = Math.Sign(direction.x);
+
+            for (int i = 1; i <= Math.Abs(direction.y * element.speed); i++)
             {
-                continue;
+                int coordinateToTest = element.position.y + i * signY;
+                if (coordinateToTest >= level.Length || level[coordinateToTest][targetX] == '#')
+                {
+                    break;
+                }
+                else
+                {
+                    targetY = coordinateToTest; 
+                }
+            }
+            
+            for (int i = 1; i <= Math.Abs(direction.x * element.speed); i++)
+            {
+                int coordinateToTest = element.position.x + i * signX;
+                if (coordinateToTest >= level[targetY].Length || level[targetY][coordinateToTest] == '#')
+                {
+                    break;
+                }
+                else
+                {
+                    targetX = coordinateToTest;
+                }
             }
 
-            element.position.x += direction.x * element.speed;
-            element.position.y += direction.y * element.speed;
+
+            element.position.x = targetX;
+            element.position.y = targetY;
 
             // HACK: First line limits y, second line limits x
             // because x useses y
             element.position.y = Math.Clamp(element.position.y, 0, level.Length - 1);
             element.position.x = Math.Clamp(element.position.x, 0, level[element.position.y].Length - 1);
 
-            // element.speed += 1;
+            element.speed += 1;
         }
     }
     else
