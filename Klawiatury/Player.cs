dae45699c24e9b2aac35c5508003ca8eaed832a0
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-23 15:56:44 +0200

    Extract moving to method in Player class

--- Klawiatury/Player.cs
+++ Klawiatury/Player.cs
@@ -18,4 +18,43 @@ class Player
         this.avatar = avatar;
         this.position = position;
     }
+
+    public void Move(Point direction, string[] level)
+    {
+        int targetX = position.x;
+        int targetY = position.y;
+
+        int signY = Math.Sign(direction.y);
+        int signX = Math.Sign(direction.x);
+
+        for (int i = 1; i <= Math.Abs(direction.y * speed); i++)
+        {
+            int coordinateToTest = position.y + i * signY;
+            if (coordinateToTest >= level.Length || coordinateToTest < 0 || level[coordinateToTest][targetX] == '#')
+            {
+                break;
+            }
+            else
+            {
+                targetY = coordinateToTest;
+            }
+        }
+
+        for (int i = 1; i <= Math.Abs(direction.x * speed); i++)
+        {
+            int coordinateToTest = position.x + i * signX;
+            if (coordinateToTest >= level[targetY].Length || coordinateToTest < 0 || level[targetY][coordinateToTest] == '#')
+            {
+                break;
+            }
+            else
+            {
+                targetX = coordinateToTest;
+            }
+        }
+
+
+        position.x = targetX;
+        position.y = targetY;
+    }
 }
--- Klawiatury/Program.cs
+++ Klawiatury/Program.cs
@@ -56,48 +56,7 @@ while (true)
 
         foreach (Player element in clones)
         {
-            // 1. Załóżmy, że docelowa pozycja początkowo to moja aktualna pozycja
-            // 2. Róbmy krok po kroku w wybranym kierunku
-            // 3. Przy każdym kroku sprawdźmy czy nie próbujemy wejść w ścianę
-            // 4a. Jeśli tak (planujemy wejść w ścianę), to przerwij
-            // 4b. Jeśli nie, to zaktualizuj docelową pozycję
-
-            int targetX = element.position.x;
-            int targetY = element.position.y;
-
-            int signY = Math.Sign(direction.y);
-            int signX = Math.Sign(direction.x);
-
-            for (int i = 1; i <= Math.Abs(direction.y * element.speed); i++)
-            {
-                int coordinateToTest = element.position.y + i * signY;
-                if (coordinateToTest >= level.Length || coordinateToTest < 0 || level[coordinateToTest][targetX] == '#')
-                {
-                    break;
-                }
-                else
-                {
-                    targetY = coordinateToTest; 
-                }
-            }
-            
-            for (int i = 1; i <= Math.Abs(direction.x * element.speed); i++)
-            {
-                int coordinateToTest = element.position.x + i * signX;
-                if (coordinateToTest >= level[targetY].Length || coordinateToTest < 0 || level[targetY][coordinateToTest] == '#')
-                {
-                    break;
-                }
-                else
-                {
-                    targetX = coordinateToTest;
-                }
-            }
-
-
-            element.position.x = targetX;
-            element.position.y = targetY;
-
+            element.Move(direction, level);
             element.speed += 1;
         }
     }
