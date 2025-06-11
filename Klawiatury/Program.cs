59428fffe15ead537f9da639fbc2f3c60287bb03
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-06-11 15:00:10 +0200

    Read fist level from text file

--- Klawiatury/Program.cs
+++ Klawiatury/Program.cs
@@ -15,7 +15,7 @@ directionsMap.Add("moveRight", new Point(1, 0));
 directionsMap.Add("moveUp", new Point(0, -1));
 directionsMap.Add("moveDown", new Point(0, 1));
 
-Level firstLevel = new Level();
+Level firstLevel = new Level(@"C:\projekty\SWPS\Semestr 2\Dzienne 2\Klawiatury\Klawiatury\firstLevel.txt");
 
 List<Character> characters = new List<Character>();
 
